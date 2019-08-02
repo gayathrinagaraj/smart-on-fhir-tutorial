b52add76dae46becb936e1b1cabac4c4c6bd45de
gayathrinagaraj <43153975+gayathrinagaraj@users.noreply.github.com>
2019-08-02 13:07:59 -0700

    Update global.js

--- example-smart-app/src/js/global.js
+++ example-smart-app/src/js/global.js
@@ -471,7 +471,7 @@ function orderStatus() {
 
 	$.ajax({
 		
-		url: "https://fhir-open.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/DocumentReference?patient="+patID+"&created=ge2019-06-01&created=le"+ todayDate,
+		url: "https://fhir-open.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/DocumentReference?patient="+patID+"&created=ge2019-04-01&created=le"+ todayDate,
 		cache: false,
 		type: "GET",
 		beforeSend: function(xhr) {
