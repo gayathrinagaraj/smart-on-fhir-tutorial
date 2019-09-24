2a80b57e8b2214200590523e8d49d98e4681b373
gayathrinagaraj <43153975+gayathrinagaraj@users.noreply.github.com>
2019-09-24 14:03:31 -0700

    Update config.js

--- example-smart-app/src/js/config.js
+++ example-smart-app/src/js/config.js
@@ -3,7 +3,7 @@
 var baseurl="https://sapphire-demo.meliorix.com/cipfhir3/baseDstu3/";
 
 //Patient Launch configurations
-var pat_clientID = '1a770187-feff-417b-9a99-aedbb4864f88';
-var pat_redirectURI= 'https://gayathrinagaraj.github.io/smart-on-fhir/example-smart-app/src/views/patient/index.html';
-var pat_scope=  'patient/Patient.read patient/Observation.read launch/patient online_access openid profile';
+var pat_clientID = "1a770187-feff-417b-9a99-aedbb4864f88";
+var pat_redirectURI= "https://gayathrinagaraj.github.io/smart-on-fhir/example-smart-app/src/views/patient/index.html";
+var pat_scope=  "patient/Patient.read patient/Observation.read launch/patient online_access openid profile";
 
