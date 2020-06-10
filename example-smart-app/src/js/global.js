997d6fc7e37dbfde1b0ac09cc1b1b5b224c40669
gayathrinagaraj <43153975+gayathrinagaraj@users.noreply.github.com>
2020-06-10 10:11:29 -0700

    Update global.js

--- example-smart-app/src/js/global.js
+++ example-smart-app/src/js/global.js
@@ -979,14 +979,11 @@ function addDays(date, days) {
 function displayList(){
 	var settings3 = {
 		
-		"async": false,
+			"async": false,
 			"crossDomain": true,
 			"url": baseurl+"ProcedureRequest?subject=http://hl7.org/fhir/sid/us-ssn/Patient/"+patID+"&_count=20&intent=order&status=active&_sort:desc=_lastUpdated",
 			"contentType" : "application/json",                                                                           
 			"cache" : false,
-		"headers": {
-				"Cache-Control": "no-cache"
-			},
 			"method": "GET"
 		
 		
