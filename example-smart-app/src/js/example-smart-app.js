9c925fdf1433b0057b5e9f007a85391ec56392c6
gayathrinagaraj <43153975+gayathrinagaraj@users.noreply.github.com>
2020-07-28 13:02:56 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -84,7 +84,7 @@ var smartObject="";
  // alert("You are using " + BrowserDetect.browser + " with version " + BrowserDetect.version + "");
 
       if(persona === 'provider') {
-	      freqOrder();
+	     // freqOrder();
 	      orderStatus();
         $('#doctor-view').show();
         if (BrowserDetect.browser.toString()!="Explorer") {
