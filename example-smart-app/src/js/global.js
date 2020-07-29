4adbaaca3e17f56f865b9c8eda1c27a6cfb5d57d
gayathrinagaraj <43153975+gayathrinagaraj@users.noreply.github.com>
2020-07-29 15:53:24 -0700

    Update global.js

--- example-smart-app/src/js/global.js
+++ example-smart-app/src/js/global.js
@@ -658,10 +658,10 @@ function listForms() {
 		for (var i=0; i < forms.length; i++) {
                 form_oid[i]=forms[i].resource.id;
                 form_name[i]=forms[i].resource.title;
-                var opt = forms[i].resource.title;
-                var val = forms[i].resource.id;
-		//var opt = form_name_test[i];
-                //var val = form_oid_test[i];
+                //var opt = forms[i].resource.title;
+                //var val = forms[i].resource.id;
+		var opt = form_name_test[i];
+                var val = form_oid_test[i];
 			
                 var el = document.createElement("option");
                                   
