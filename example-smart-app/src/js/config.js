fea2d43056c85ed8b4d05c818a7734400d02eb45
gayathrinagaraj <43153975+gayathrinagaraj@users.noreply.github.com>
2021-02-24 12:49:23 -0800

    Update config.js

--- example-smart-app/src/js/config.js
+++ example-smart-app/src/js/config.js
@@ -1,11 +1,9 @@
 //Configuration File
 //Hapi FHIR server base URL
 //var baseurl="https://calv-easiproapp.med.usc.edu:8082/baseDstu3/";
-//var baseurl="https://calv-easiprox.med.usc.edu/fhir3/baseDstu3/";
+var baseurl="https://calv-easiprox.med.usc.edu/fhir3/baseDstu3/";
 
-//var baseurl_easipro="https://calv-easiprox.med.usc.edu/easipro/";
-
-var baseurl="https://fhir3-stage.elimuinformatics.com/baseDstu3/";
+//var baseurl="https://fhir3-stage.elimuinformatics.com/baseDstu3/";
 
 var baseurl_acapi ="https://assessmentcenter.net/ac_api/";
 
